7e8e8edd374d3ad37b2bf5f176bfaf11fe9b03c8
Zuzanna Szymańska <85112360+Zuzanna-Szymanska@users.noreply.github.com>
2021-10-17 22:18:35 +0200

    Update convergence_3D.py

--- convergence_3D.py
+++ convergence_3D.py
@@ -21,6 +21,8 @@
 ### to be published in October 2021 by Cambridge University Press
 ### https://www.cambridge.org/pl/academic/subjects/mathematics/differential-and-integral-equations-dynamical-systems-and-co/spaces-measures-and-their-applications-structured-population-models?format=HB
 ###
+### PLEASE SEE THE README.md TO KNOW HOW TO CITE.
+###
 ##################################################################################################
 ##################################################################################################
 
